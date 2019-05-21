ccd6e02ba6eea61e1ebe10d8b5e9452bd82f0447
Angelo Breuer <angelobr@outlook.de>
2019-05-21 21:04:06 +0200

    Removed testing code.

--- src/Lavalink4NET/LavalinkSocket.cs
+++ src/Lavalink4NET/LavalinkSocket.cs
@@ -1,21 +1,21 @@
-/* 
+/*
  *  File:   LavalinkSocket.cs
  *  Author: Angelo Breuer
- *  
+ *
  *  The MIT License (MIT)
- *  
+ *
  *  Copyright (c) Angelo Breuer 2019
- *  
+ *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
- *  
+ *
  *  The above copyright notice and this permission notice shall be included in
  *  all copies or substantial portions of the Software.
- *  
+ *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -294,12 +294,6 @@ namespace Lavalink4NET
                 Logger?.LogTrace("Sent payload `{Payload}` to {Uri}.", content, _webSocketUri);
             }
         }
-
-        /// <summary>
-        ///     TODO
-        /// </summary>
-        public void EmulateConnectionLoss()
-            => _webSocket.CloseAsync(WebSocketCloseStatus.Empty, string.Empty, CancellationToken.None);
 
         /// <summary>
         ///     Ensures that the socket is initialized.
