48fa24ac80dfa6b2ef38031ec5d7d9a27de4f882
Angelo Breuer <angelobr@outlook.de>
2019-06-19 22:18:11 +0200

    Added track decoding tests

---
+++ src/Lavalink4NET.Tests/TrackDecodingTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Lavalink4NET.Tests
+{
+    using Lavalink4NET.Decoding;
+    using Xunit;
+
+    public sealed class TrackDecodingTest
+    {
+        [Theory]
+        [InlineData("QAAAjAIAJFZhbmNlIEpveSAtICdSaXB0aWRlJyBPZmZpY2lhbCBWaWRlbwAObXVzaHJvb212aWRlb3MAAAAAAAMgyAALdUpfMUhNQUdiNGsAAQAraHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj11Sl8xSE1BR2I0awAHeW91dHViZQAAAAAAAAAA")]
+        [InlineData("QAAAjQIAK1JpdGEgT3JhIC0gWW91ciBTb25nIChPZmZpY2lhbCBMeXJpYyBWaWRlbykACFJpdGEgT3JhAAAAAAACwwgAC2k5NU5sYjdraVBvAAEAK2h0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9aTk1TmxiN2tpUG8AB3lvdXR1YmUAAAAAAAAAAA==")]
+        [InlineData("QAAAmAIAMkx1a2FzIEdyYWhhbSAtIExvdmUgU29tZW9uZSBbT0ZGSUNJQUwgTVVTSUMgVklERU9dAAxMdWthcyBHcmFoYW0AAAAAAAOhsAALZE40NHhwSGpOeEUAAQAraHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kTjQ0eHBIak54RQAHeW91dHViZQAAAAAAAAAA")]
+        public void TestTrackDecoding(string base64)
+        {
+            // verify the header of the base64 encoded track
+            TrackDecoder.DecodeTrack(base64, verify: true);
+        }
+    }
+}
