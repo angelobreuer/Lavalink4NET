8418e490d9308ea7267de111c3970dffc5e24a85
Angelo Breuer <angelobr@outlook.de>
2019-06-19 21:43:25 +0200

    Added memory cache tests

---
+++ src/Lavalink4NET.Tests/MemoryCacheTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Lavalink4NET.Tests
+{
+    using System;
+    using Lavalink4NET.MemoryCache;
+    using Xunit;
+
+    public class MemoryCacheTests
+    {
+        [Fact]
+        public void TestMemoryCache()
+        {
+            using (var cache = new LavalinkCache())
+            {
+                // no item in cache
+                Assert.False(cache.TryGetItem<string>("key", out _));
+
+                // add item to cache
+                cache.AddItem("key", string.Empty, DateTimeOffset.UtcNow + TimeSpan.FromSeconds(10));
+
+                // item in cache
+                Assert.True(cache.TryGetItem<string>("key", out _));
+            }
+        }
+    }
+}
