4b6980e821877f0695a8f8c9fc05bd36587b0ee7
Angelo Breuer <angelobr@outlook.de>
2019-06-19 21:03:30 +0200

    Fixed payload converter

--- src/Lavalink4NET/Payloads/PayloadConverter.cs
+++ src/Lavalink4NET/Payloads/PayloadConverter.cs
@@ -1,21 +1,21 @@
-/* 
+/*
  *  File:   PayloadConverter.cs
  *  Author: Angelo Breuer
- *  
+ *
  *  The MIT License (MIT)
- *  
+ *
  *  Copyright (c) Angelo Breuer 2019
- *  
+ *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
- *  
+ *
  *  The above copyright notice and this permission notice shall be included in
  *  all copies or substantial portions of the Software.
- *  
+ *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,6 +28,9 @@
 namespace Lavalink4NET.Payloads
 {
     using System;
+    using Lavalink4NET.Payloads.Events;
+    using Lavalink4NET.Payloads.Node;
+    using Lavalink4NET.Payloads.Player;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
 
@@ -96,7 +99,7 @@ namespace Lavalink4NET.Payloads
         {
             var jObject = JsonConvert.DeserializeObject<JObject>(json);
 
-            if (!jObject.TryGetValue("op", out JToken opCodeToken))
+            if (!jObject.TryGetValue("op", out var opCodeToken))
             {
                 throw new Exception("Invalid JSON: Expected 'op' in json object.");
             }
@@ -105,7 +108,7 @@ namespace Lavalink4NET.Payloads
 
             if (opCode == OpCode.Event)
             {
-                if (!jObject.TryGetValue("type", out JToken eventTypeToken))
+                if (!jObject.TryGetValue("type", out var eventTypeToken))
                 {
                     throw new Exception("Invalid JSON: Expected 'type' in json object.");
                 }
