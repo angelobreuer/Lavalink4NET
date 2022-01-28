a1891d2f00316d49ad3c0e2fb8189fcf6db9ab6b
Angelo Breuer <angelobr@outlook.de>
2022-01-28 14:47:51 +0100

    Tests

---
+++ src/Lavalink4NET.Tests/DiscordClientWrapperTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Lavalink4NET.Tests;
+using System;
+using Discord.WebSocket;
+using Lavalink4NET.DiscordNet;
+using Xunit;
+
+public sealed class DiscordClientWrapperTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-100)]
+    public void TestConstructorThrowsIfInvalidShardCountPassed(int shards)
+    {
+        var client = new DiscordShardedClient();
+        Assert.Throws<ArgumentOutOfRangeException>(() => new DiscordClientWrapper(client, shards));
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(20)]
+    [InlineData(100)]
+    public void TestConstructorDoesNotThrowIfValidShardCountPassed(int shards)
+    {
+        var client = new DiscordShardedClient();
+        _ = new DiscordClientWrapper(client, shards);
+    }
+
+    [Fact]
+    public void TestUnavailableAccessThrows()
+    {
+        var client = new DiscordShardedClient();
+
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            _ = new DiscordClientWrapper(client).ShardCount;
+        });
+    }
+}
