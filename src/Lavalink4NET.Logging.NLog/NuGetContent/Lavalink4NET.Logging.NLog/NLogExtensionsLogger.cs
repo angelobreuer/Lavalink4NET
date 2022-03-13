f357f7428aa3bb0db6aad01812b84b88c58de3e4
Angelo Breuer <angelobr@outlook.de>
2022-03-13 10:41:10 +0100

    Add Lavalink4NET.Logging.NLog

---
+++ src/Lavalink4NET.Logging.NLog/NuGetContent/Lavalink4NET.Logging.NLog/NLogExtensionsLogger.cs
@@ -0,0 +1,39 @@
+﻿namespace Lavalink4NET.Logging.NLog;
+
+using System;
+using System.Runtime.CompilerServices;
+using global::NLog;
+
+internal sealed class NLogExtensionsLogger : Logging.ILogger
+{
+    private readonly ConditionalWeakTable<object, ILogger> _loggerCache;
+    private readonly LogFactory _logFactory;
+
+    public NLogExtensionsLogger(LogFactory logFactory)
+    {
+        _logFactory = logFactory ?? throw new ArgumentNullException(nameof(logFactory));
+        _loggerCache = new ConditionalWeakTable<object, ILogger>();
+    }
+
+    /// <inheritdoc/>
+    public void Log(object source, string message, Logging.LogLevel level = Logging.LogLevel.Information, Exception? exception = null)
+    {
+        var logger = _loggerCache.GetValue(source, source => _logFactory.GetCurrentClassLogger(source.GetType()));
+
+        var logLevel = level switch
+        {
+            Logging.LogLevel.Information => LogLevel.Info,
+            Logging.LogLevel.Error => LogLevel.Error,
+            Logging.LogLevel.Warning => LogLevel.Warn,
+            Logging.LogLevel.Debug => LogLevel.Debug,
+            Logging.LogLevel.Trace => LogLevel.Trace,
+
+            _ => throw new ArgumentOutOfRangeException(
+                paramName: nameof(level),
+                actualValue: level,
+                message: "The specified log level does not exist."),
+        };
+
+        logger.Log(logLevel, exception, message, args: Array.Empty<object>());
+    }
+}
---
+++ src/Lavalink4NET.Logging.NLog/NuGetContent/Lavalink4NET.Logging.NLog/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Lavalink4NET.Logging.NLog;
+
+using global::Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddNLogLavalinkLogging(this IServiceCollection services)
+    {
+        return services.AddSingleton<ILogger, NLogExtensionsLogger>();
+    }
+}
