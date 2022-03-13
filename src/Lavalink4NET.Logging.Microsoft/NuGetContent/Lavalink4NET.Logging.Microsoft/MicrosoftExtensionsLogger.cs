f98f541150462249df006b991e84c627e1476c20
Angelo Breuer <angelobr@outlook.de>
2022-03-13 10:40:56 +0100

    Add Lavalink4NET.Logging.Microsoft

---
+++ src/Lavalink4NET.Logging.Microsoft/NuGetContent/Lavalink4NET.Logging.Microsoft/MicrosoftExtensionsLogger.cs
@@ -0,0 +1,39 @@
+﻿namespace Lavalink4NET.Logging.Microsoft;
+
+using System;
+using System.Runtime.CompilerServices;
+using global::Microsoft.Extensions.Logging;
+
+internal sealed class MicrosoftExtensionsLogger : Logging.ILogger
+{
+    private readonly ConditionalWeakTable<object, ILogger> _loggerCache;
+    private readonly ILoggerFactory _loggerFactory;
+
+    public MicrosoftExtensionsLogger(ILoggerFactory loggerFactory)
+    {
+        _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+        _loggerCache = new ConditionalWeakTable<object, ILogger>();
+    }
+
+    /// <inheritdoc/>
+    public void Log(object source, string message, Logging.LogLevel level = Logging.LogLevel.Information, Exception? exception = null)
+    {
+        var logger = _loggerCache.GetValue(source, source => _loggerFactory.CreateLogger(source.GetType()));
+
+        var logLevel = level switch
+        {
+            Logging.LogLevel.Information => LogLevel.Information,
+            Logging.LogLevel.Error => LogLevel.Error,
+            Logging.LogLevel.Warning => LogLevel.Warning,
+            Logging.LogLevel.Debug => LogLevel.Debug,
+            Logging.LogLevel.Trace => LogLevel.Trace,
+
+            _ => throw new ArgumentOutOfRangeException(
+                paramName: nameof(level),
+                actualValue: level,
+                message: "The specified log level does not exist."),
+        };
+
+        logger.Log(logLevel, exception, message, args: Array.Empty<object>());
+    }
+}
---
+++ src/Lavalink4NET.Logging.Microsoft/NuGetContent/Lavalink4NET.Logging.Microsoft/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Lavalink4NET.Logging.Microsoft;
+
+using global::Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMicrosoftExtensionsLavalinkLogging(this IServiceCollection services)
+    {
+        return services.AddSingleton<ILogger, MicrosoftExtensionsLogger>();
+    }
+}
