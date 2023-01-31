29a808f8dc4b9f841187721b5e3f1768cafa6536
Angelo Breuer <angelobr@outlook.de>
2023-01-31 13:39:15 +0100

    tests(protocol): First tests

---
+++ tests/Lavalink4NET.Protocol.Tests/Payloads/ReadyPayloadTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Lavalink4NET.Protocol.Tests.Payloads;
+
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+public sealed class ReadyPayloadTests
+{
+    [Fact]
+    public void TestRoundtrip()
+    {
+        // Arrange
+        var node = JsonNode.Parse("""
+            {
+              "op": "ready",
+              "resumed": false,
+              "sessionId": "..."
+            }
+            """);
+
+        // Act
+        var payload = node.Deserialize(ProtocolSerializerContext.Default.IPayload);
+        var serializedString = JsonSerializer.Serialize(payload);
+
+        // Assert
+        Assert.Equal(node!.ToJsonString(), serializedString);
+    }
+}
