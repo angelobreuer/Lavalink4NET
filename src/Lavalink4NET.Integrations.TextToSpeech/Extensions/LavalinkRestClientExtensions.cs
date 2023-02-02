f171dc5b95cd9e6025fecbf0527c81f113e8cadf
Angelo Breuer <angelobr@outlook.de>
2023-02-02 11:42:27 +0100

    fix: Fix integrations

--- src/Lavalink4NET.Integrations.TextToSpeech/Extensions/LavalinkRestClientExtensions.cs
+++ src/Lavalink4NET.Integrations.TextToSpeech/Extensions/LavalinkRestClientExtensions.cs
@@ -1,33 +1,54 @@
 ﻿namespace Lavalink4NET.Rest;
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Lavalink4NET.Integrations.TextToSpeech;
 using Lavalink4NET.Integrations.TextToSpeech.Synthesis;
+using Lavalink4NET.Rest.Entities;
+using Lavalink4NET.Rest.Entities.Tracks;
 using Lavalink4NET.Tracks;
 
 public static class LavalinkRestClientExtensions
 {
     public static ValueTask<LavalinkTrack?> GetTextToSpeechTrackAsync(
-        this ILavalinkRestClient restClient,
+        this ILavalinkApiClient apiClient,
         string textToSpeech,
         string? languageName = null,
-        bool noCache = false,
+        CacheMode cacheMode = CacheMode.Dynamic,
         CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(apiClient);
+        ArgumentNullException.ThrowIfNull(textToSpeech);
+
         var encodedUri = TextToSpeechTrackEncoder.EncodeMessage(textToSpeech, languageName);
-        return restClient.GetTrackAsync(encodedUri.ToString(), mode: SearchMode.None, noCache, cancellationToken);
+
+        var loadOptions = new TrackLoadOptions(
+            SearchMode: TrackSearchMode.None,
+            StrictSearch: false,
+            CacheMode: cacheMode);
+
+        return apiClient.LoadTrackAsync(encodedUri.ToString(), loadOptions, cancellationToken);
     }
 
     public static ValueTask<LavalinkTrack?> GetTextToSpeechTrackAsync(
-        this ILavalinkRestClient restClient,
+        this ILavalinkApiClient apiClient,
         SynthesisInput input,
         VoiceSelectionParameters? voiceSelectionParameters = null,
         AudioConfiguration? audioConfiguration = null,
-        bool noCache = false,
+        CacheMode cacheMode = CacheMode.Dynamic,
         CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(apiClient);
+        ArgumentNullException.ThrowIfNull(input);
+
         var encodedUri = TextToSpeechTrackEncoder.EncodeMessage(input, voiceSelectionParameters, audioConfiguration);
-        return restClient.GetTrackAsync(encodedUri.OriginalString, mode: SearchMode.None, noCache, cancellationToken);
+
+        var loadOptions = new TrackLoadOptions(
+            SearchMode: TrackSearchMode.None,
+            StrictSearch: false,
+            CacheMode: cacheMode);
+
+        return apiClient.LoadTrackAsync(encodedUri.ToString(), loadOptions, cancellationToken);
     }
 }
