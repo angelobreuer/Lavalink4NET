268f8555449ec31fa42a9b7b3c2d15d9c7cb3e88
Angelo Breuer <angelobr@outlook.de>
2023-02-04 22:23:57 +0100

    feat: Add YouTube music search mode

--- src/Lavalink4NET.Rest/Entities/Tracks/TrackSearchMode.cs
+++ src/Lavalink4NET.Rest/Entities/Tracks/TrackSearchMode.cs
@@ -3,6 +3,7 @@
 public readonly record struct TrackSearchMode(string? Prefix)
 {
     public static readonly TrackSearchMode YouTube = new("ytsearch");
+    public static readonly TrackSearchMode YouTubeMusic = new("ytmsearch");
     public static readonly TrackSearchMode SoundCloud = new("scsearch");
     public static readonly TrackSearchMode None = new(null);
 }
