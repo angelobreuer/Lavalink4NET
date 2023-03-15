aa727d97fb7e380a4040212c5448edece96d5b80
Angelo Breuer <angelobr@outlook.de>
2023-03-15 18:08:04 +0100

    feat: Expose LavalinkApiEndpoints#Build

--- src/Lavalink4NET.Rest/LavalinkApiEndpoints.cs
+++ src/Lavalink4NET.Rest/LavalinkApiEndpoints.cs
@@ -6,8 +6,6 @@ public sealed class LavalinkApiEndpoints
 {
     public const int CurrentVersion = 4;
 
-    private Uri Build(string relativeUri) => new(BaseAddress, relativeUri);
-
     public LavalinkApiEndpoints(Uri baseAddress, int version = CurrentVersion)
     {
         ArgumentNullException.ThrowIfNull(baseAddress);
@@ -26,21 +24,23 @@ public sealed class LavalinkApiEndpoints
         LoadTracks = Build($"v{version}/loadtracks");
     }
 
-    public Uri BaseAddress { get; }
-
     public int ApiVersion { get; }
 
-    public Uri Version { get; }
+    public Uri BaseAddress { get; }
 
-    public Uri WebSocket { get; }
+    public Uri Information { get; }
+
+    public Uri LoadTracks { get; }
 
     public Uri Sessions { get; }
 
     public Uri Statistics { get; }
 
-    public Uri Information { get; }
+    public Uri Version { get; }
 
-    public Uri LoadTracks { get; }
+    public Uri WebSocket { get; }
+
+    public Uri Build(string relativeUri) => new(BaseAddress, relativeUri);
 
     public Uri Player(string sessionId, ulong guildId)
     {
