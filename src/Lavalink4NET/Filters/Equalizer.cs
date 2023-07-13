f991af5a2fb8f8a49e85455187c892a38b75d8e8
Angelo Breuer <angelobr@outlook.de>
2023-07-13 14:05:38 +0200

    feat: Overhaul equalizer type

--- src/Lavalink4NET/Filters/Equalizer.cs
+++ src/Lavalink4NET/Filters/Equalizer.cs
@@ -1,86 +1,257 @@
 ﻿namespace Lavalink4NET.Filters;
 
 using System;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[StructLayout(LayoutKind.Explicit, Pack = 0, Size = Bands * 2)]
-public readonly record struct Equalizer
+[StructLayout(LayoutKind.Sequential, Size = EqualizerData.Size)]
+public sealed record class Equalizer
 {
     public const int Bands = 15; // 0-14
 
-    [FieldOffset(0)]
-    private readonly ushort _data;
+    private readonly EqualizerData _data;
 
-    public static Equalizer Default => default;
+    public Equalizer() : this(data: default)
+    {
+    }
 
-    public float Band0 { get => this[0]; init => this[0] = value; }
-    public float Band1 { get => this[1]; init => this[1] = value; }
-    public float Band2 { get => this[2]; init => this[2] = value; }
-    public float Band3 { get => this[3]; init => this[3] = value; }
-    public float Band4 { get => this[4]; init => this[4] = value; }
-    public float Band5 { get => this[5]; init => this[5] = value; }
-    public float Band6 { get => this[6]; init => this[6] = value; }
-    public float Band7 { get => this[7]; init => this[7] = value; }
-    public float Band8 { get => this[8]; init => this[8] = value; }
-    public float Band9 { get => this[9]; init => this[9] = value; }
-    public float Band10 { get => this[10]; init => this[10] = value; }
-    public float Band11 { get => this[11]; init => this[11] = value; }
-    public float Band12 { get => this[12]; init => this[12] = value; }
-    public float Band13 { get => this[13]; init => this[13] = value; }
-    public float Band14 { get => this[14]; init => this[14] = value; }
+    private Equalizer(EqualizerData data)
+    {
+        _data = data;
+    }
+
+    public static Equalizer Default { get; } = new();
+
+    public float Band0
+    {
+        get => this[0];
+        init => this[0] = value;
+    }
+
+    public float Band1
+    {
+        get => this[1];
+        init => this[1] = value;
+    }
+
+    public float Band2
+    {
+        get => this[2];
+        init => this[2] = value;
+    }
+
+    public float Band3
+    {
+        get => this[3];
+        init => this[3] = value;
+    }
+
+    public float Band4
+    {
+        get => this[4];
+        init => this[4] = value;
+    }
+
+    public float Band5
+    {
+        get => this[5];
+        init => this[5] = value;
+    }
+
+    public float Band6
+    {
+        get => this[6];
+        init => this[6] = value;
+    }
+
+    public float Band7
+    {
+        get => this[7];
+        init => this[7] = value;
+    }
+
+    public float Band8
+    {
+        get => this[8];
+        init => this[8] = value;
+    }
+
+    public float Band9
+    {
+        get => this[9];
+        init => this[9] = value;
+    }
+
+    public float Band10
+    {
+        get => this[10];
+        init => this[10] = value;
+    }
+
+    public float Band11
+    {
+        get => this[11];
+        init => this[11] = value;
+    }
+
+    public float Band12
+    {
+        get => this[12];
+        init => this[12] = value;
+    }
+
+    public float Band13
+    {
+        get => this[13];
+        init => this[13] = value;
+    }
+
+    public float Band14
+    {
+        get => this[14];
+        init => this[14] = value;
+    }
 
     public float this[int band]
     {
-        get
-        {
-            CheckIndex(band);
-
-            var rawValue = Unsafe.Add(
-                source: ref Unsafe.AsRef(_data),
-                elementOffset: band);
-
-            if (rawValue is 0)
-            {
-                return 0F; // fast path
-            }
-
-            var mappedValue = rawValue / (float)ushort.MaxValue * 1.25F;
-            return mappedValue > 1.0F ? mappedValue - 1.5F : mappedValue;
-        }
-
-        init
-        {
-            CheckIndex(band);
-
-            // normalize
-            value = Math.Max(-0.25F, value);
-            value = Math.Min(1.0F, value);
-
-            // map
-            if (value < 0F)
-            {
-                value = -value + 1.0F;
-            }
-
-            // stretch
-            var rawValue = (ushort)Math.Round(value * ushort.MaxValue);
-
-            // encode
-            Unsafe.Add(
-                source: ref _data,
-                elementOffset: band) = rawValue;
-        }
+        get => _data[band];
+        init => _data[band] = value;
     }
 
-    private static void CheckIndex(int band)
+    public static Builder CreateBuilder(Equalizer equalizer) => new(equalizer._data);
+
+    public static Builder CreateBuilder() => default;
+
+    public struct Builder
     {
-        if (band is < 0 or >= Bands)
+        private EqualizerData _equalizer;
+
+        internal Builder(EqualizerData data)
+        {
+            _equalizer = data;
+        }
+
+        public float this[int band]
+        {
+            get => _equalizer[band];
+            set => _equalizer[band] = value;
+        }
+
+        public float Band0
+        {
+            get => this[0];
+            set => this[0] = value;
+        }
+
+        public float Band1
+        {
+            get => this[1];
+            set => this[1] = value;
+        }
+
+        public float Band2
+        {
+            get => this[2];
+            set => this[2] = value;
+        }
+
+        public float Band3
+        {
+            get => this[3];
+            set => this[3] = value;
+        }
+
+        public float Band4
+        {
+            get => this[4];
+            set => this[4] = value;
+        }
+
+        public float Band5
+        {
+            get => this[5];
+            set => this[5] = value;
+        }
+
+        public float Band6
+        {
+            get => this[6];
+            set => this[6] = value;
+        }
+
+        public float Band7
+        {
+            get => this[7];
+            set => this[7] = value;
+        }
+
+        public float Band8
+        {
+            get => this[8];
+            set => this[8] = value;
+        }
+
+        public float Band9
+        {
+            get => this[9];
+            set => this[9] = value;
+        }
+
+        public float Band10
+        {
+            get => this[10];
+            set => this[10] = value;
+        }
+
+        public float Band11
+        {
+            get => this[11];
+            set => this[11] = value;
+        }
+
+        public float Band12
+        {
+            get => this[12];
+            set => this[12] = value;
+        }
+
+        public float Band13
+        {
+            get => this[13];
+            set => this[13] = value;
+        }
+
+        public float Band14
+        {
+            get => this[14];
+            set => this[14] = value;
+        }
+
+        public readonly Equalizer Build() => new(_equalizer);
+    }
+}
+
+internal struct EqualizerData
+{
+    public const int Size = Equalizer.Bands * sizeof(float);
+
+    public unsafe fixed float Data[Equalizer.Bands];
+
+    private unsafe ref float GetBand(int band)
+    {
+        if (band is < 0 or >= Equalizer.Bands)
         {
             throw new ArgumentOutOfRangeException(
                 paramName: nameof(band),
                 actualValue: band,
-                message: $"The band index must be between zero and {Bands - 1}");
+                message: $"The band index must be between zero and {Equalizer.Bands - 1}");
         }
+
+        return ref Data[band];
+    }
+
+    public float this[int band]
+    {
+        get => GetBand(band);
+        set => GetBand(band) = Math.Clamp(value, -0.25F, 1.0F);
     }
 }
