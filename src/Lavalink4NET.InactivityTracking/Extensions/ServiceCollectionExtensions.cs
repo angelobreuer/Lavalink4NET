7ffbbb28a6b454b9fe6286454d0b189fd82c3875
Angelo Breuer <angelobr@outlook.de>
2023-08-14 01:52:05 +0200

    feat: Implement ConfigureInactivityTracking

--- src/Lavalink4NET.InactivityTracking/Extensions/ServiceCollectionExtensions.cs
+++ src/Lavalink4NET.InactivityTracking/Extensions/ServiceCollectionExtensions.cs
@@ -6,6 +6,7 @@ using Lavalink4NET.InactivityTracking.Hosting;
 using Lavalink4NET.Tracking;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
 
 public static class ServiceCollectionExtensions
 {
@@ -19,5 +20,42 @@ public static class ServiceCollectionExtensions
         services.AddHostedService<InactivityTrackingServiceHost>();
 
         return services;
+    }
+
+    public static IServiceCollection ConfigureInactivityTracking(this IServiceCollection services, Action<InactivityTrackingOptions> configure)
+    {
+        services.Configure(configure);
+
+        return services;
+    }
+
+    public static IServiceCollection ConfigureInactivityTracking(this IServiceCollection services, Action<IServiceProvider, InactivityTrackingOptions> configure)
+    {
+        services.AddSingleton<IConfigureOptions<InactivityTrackingOptions>>(
+            serviceProvider => new ConfigureOptions<InactivityTrackingOptions>(serviceProvider, configure));
+
+        return services;
+    }
+}
+
+file sealed class ConfigureOptions<T> : IConfigureOptions<T> where T : class
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly Action<IServiceProvider, T> _action;
+
+    public ConfigureOptions(IServiceProvider serviceProvider, Action<IServiceProvider, T> action)
+    {
+        ArgumentNullException.ThrowIfNull(serviceProvider);
+        ArgumentNullException.ThrowIfNull(action);
+
+        _serviceProvider = serviceProvider;
+        _action = action;
+    }
+
+    public void Configure(T options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        _action(_serviceProvider, options);
     }
 }
