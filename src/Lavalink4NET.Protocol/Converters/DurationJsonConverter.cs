135d5d91fe4074b6e4c66bfe0a9efdee2a6f2c97
Angelo Breuer <angelobr@outlook.de>
2023-08-14 07:25:27 +0200

    fix: Fix duration JSON converter when handling live tracks

--- src/Lavalink4NET.Protocol/Converters/DurationJsonConverter.cs
+++ src/Lavalink4NET.Protocol/Converters/DurationJsonConverter.cs
@@ -11,13 +11,26 @@ internal sealed class DurationJsonConverter : JsonConverter<TimeSpan>
         ArgumentNullException.ThrowIfNull(typeToConvert);
         ArgumentNullException.ThrowIfNull(options);
 
-        return TimeSpan.FromMilliseconds(reader.GetInt64());
+        var value = reader.GetInt64();
+
+        if (value is long.MaxValue)
+        {
+            return TimeSpan.MaxValue;
+        }
+
+        return TimeSpan.FromMilliseconds(value);
     }
 
     public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
     {
         ArgumentNullException.ThrowIfNull(writer);
         ArgumentNullException.ThrowIfNull(options);
+
+        if (value == TimeSpan.MaxValue)
+        {
+            writer.WriteNumberValue(long.MaxValue);
+            return;
+        }
 
         writer.WriteNumberValue((long)Math.Round(value.TotalMilliseconds));
     }
