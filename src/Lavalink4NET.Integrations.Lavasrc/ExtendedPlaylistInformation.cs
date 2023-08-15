119e1f401ccd1ab2627d123bff79f48d961817d1
Angelo Breuer <angelobr@outlook.de>
2023-08-15 20:19:42 +0200

    feat: Implement extended playlist information type

---
+++ src/Lavalink4NET.Integrations.Lavasrc/ExtendedPlaylistInformation.cs
@@ -0,0 +1,61 @@
+﻿namespace Lavalink4NET.Integrations.Lavasrc;
+
+using System.Collections.Immutable;
+using System.Text.Json.Nodes;
+using Lavalink4NET.Rest.Entities.Tracks;
+
+public readonly record struct ExtendedPlaylistInformation(PlaylistInformation Playlist)
+{
+    public string Name => Playlist.Name;
+
+    public ExtendedLavalinkTrack? SelectedTrack
+    {
+        get
+        {
+            return Playlist.SelectedTrack is null
+                ? null
+                : new ExtendedLavalinkTrack(Playlist.SelectedTrack);
+        }
+    }
+
+    public IImmutableDictionary<string, JsonNode> AdditionalInformation => Playlist.AdditionalInformation;
+
+    public PlaylistType? Type
+    {
+        get
+        {
+            var playlistType = AdditionalInformation["type"]?.ToString();
+
+            return playlistType switch
+            {
+                "album" => PlaylistType.Album,
+                "playlist" => PlaylistType.Playlist,
+                "artist" => PlaylistType.Artist,
+                "recommendations" => PlaylistType.Recommendations,
+                _ => null
+            };
+        }
+    }
+
+    public Uri? Uri
+    {
+        get
+        {
+            var uri = AdditionalInformation["url"]?.ToString();
+            return uri is null ? null : new Uri(uri);
+        }
+    }
+
+    public Uri? ArtworkUri
+    {
+        get
+        {
+            var artworkUri = AdditionalInformation["artworkUrl"]?.ToString();
+            return artworkUri is null ? null : new Uri(artworkUri);
+        }
+    }
+
+    public string? Author => AdditionalInformation["author"]?.ToString();
+
+    public int? TotalTracks => AdditionalInformation["totalTracks"]?.GetValue<int>();
+}
