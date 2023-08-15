669ef2406565d71027c53b249210d41af881a05c
Angelo Breuer <angelobr@outlook.de>
2023-08-15 19:43:09 +0200

    feat: Split Lavalink track decoding into own type

--- src/Lavalink4NET.Abstractions/Tracks/LavalinkTrack.IParsable.cs
+++ src/Lavalink4NET.Abstractions/Tracks/LavalinkTrack.IParsable.cs
@@ -2,11 +2,9 @@
 
 using System;
 using System.Buffers;
-using System.Buffers.Binary;
 using System.Buffers.Text;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
 using System.Text.Unicode;
 
 public partial record class LavalinkTrack
@@ -27,6 +25,89 @@ public partial record class LavalinkTrack
     public static LavalinkTrack Parse(string s, IFormatProvider? provider)
     {
         return Parse(s.AsSpan(), provider);
+    }
+
+    internal static bool TryParse(ReadOnlySpan<char> originalTrackData, ref LavalinkTrackDecoder trackDecoder, [MaybeNullWhen(false)] out LavalinkTrack result)
+    {
+        result = null;
+
+        if (!trackDecoder.TryReadHeader(out var version) ||
+            !trackDecoder.TryReadString(out var title) ||
+            !trackDecoder.TryReadString(out var author) ||
+            !trackDecoder.TryReadInt64(out var durationValue) ||
+            !trackDecoder.TryReadString(out var identifier) ||
+            !trackDecoder.TryReadBoolean(out var isStream) ||
+            !trackDecoder.TryReadOptionalString(out var rawUri))
+        {
+            return false;
+        }
+
+        var rawArtworkUri = default(string?);
+        var isrc = default(string?);
+
+        if (version >= 3 && (!trackDecoder.TryReadOptionalString(out rawArtworkUri) || !trackDecoder.TryReadOptionalString(out isrc)))
+        {
+            return false;
+        }
+
+        var uri = default(Uri?);
+        if (rawUri is not null && !Uri.TryCreate(rawUri, UriKind.Absolute, out uri))
+        {
+            return false;
+        }
+
+        var artworkUri = default(Uri?);
+        if (rawArtworkUri is not null && !Uri.TryCreate(rawArtworkUri, UriKind.Absolute, out artworkUri))
+        {
+            return false;
+        }
+
+        if (!trackDecoder.TryReadString(out var sourceName))
+        {
+            return false;
+        }
+
+        var isProbingAudioTrack =
+            sourceName.Equals("http", StringComparison.OrdinalIgnoreCase) ||
+            sourceName.Equals("local", StringComparison.OrdinalIgnoreCase);
+
+        var containerProbeInformation = default(string?);
+        if (isProbingAudioTrack && !trackDecoder.TryReadString(out containerProbeInformation))
+        {
+            return false;
+        }
+
+        if (!trackDecoder.TryReadInt64(out var startPositionValue))
+        {
+            return false;
+        }
+
+        var startPosition = startPositionValue is 0
+            ? default(TimeSpan?)
+            : TimeSpan.FromMilliseconds(startPositionValue);
+
+        var duration = durationValue == long.MaxValue
+            ? TimeSpan.MaxValue
+            : TimeSpan.FromMilliseconds(durationValue);
+
+        result = new LavalinkTrack
+        {
+            Author = author,
+            Identifier = identifier,
+            Title = title,
+            Duration = duration,
+            IsLiveStream = isStream,
+            IsSeekable = !isStream,
+            ProbeInfo = containerProbeInformation,
+            SourceName = sourceName,
+            StartPosition = startPosition,
+            Uri = uri,
+            ArtworkUri = artworkUri,
+            Isrc = isrc,
+            TrackData = originalTrackData.ToString(),
+        };
+
+        return true;
     }
 
     public static bool TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, [MaybeNullWhen(false)] out LavalinkTrack result)
@@ -71,199 +152,8 @@ public partial record class LavalinkTrack
 
     private static bool TryParse(ReadOnlySpan<char> originalTrackData, ReadOnlySpan<byte> buffer, [MaybeNullWhen(false)] out LavalinkTrack result)
     {
-        result = null;
-
-        if (!TryReadHeader(ref buffer, out var version) ||
-            !TryReadString(ref buffer, out var title) ||
-            !TryReadString(ref buffer, out var author) ||
-            !TryReadInt64(ref buffer, out var durationValue) ||
-            !TryReadString(ref buffer, out var identifier) ||
-            !TryReadBoolean(ref buffer, out var isStream) ||
-            !TryReadOptionalString(ref buffer, out var rawUri))
-        {
-            return false;
-        }
-
-        var rawArtworkUri = default(string?);
-        var isrc = default(string?);
-
-        if (version >= 3 && (!TryReadOptionalString(ref buffer, out rawArtworkUri) || !TryReadOptionalString(ref buffer, out isrc)))
-        {
-            return false;
-        }
-
-        var uri = default(Uri?);
-        if (rawUri is not null && !Uri.TryCreate(rawUri, UriKind.Absolute, out uri))
-        {
-            return false;
-        }
-
-        var artworkUri = default(Uri?);
-        if (rawArtworkUri is not null && !Uri.TryCreate(rawArtworkUri, UriKind.Absolute, out artworkUri))
-        {
-            return false;
-        }
-
-        if (!TryReadString(ref buffer, out var sourceName))
-        {
-            return false;
-        }
-
-        var isProbingAudioTrack =
-            sourceName.Equals("http", StringComparison.OrdinalIgnoreCase) ||
-            sourceName.Equals("local", StringComparison.OrdinalIgnoreCase);
-
-        var containerProbeInformation = default(string?);
-        if (isProbingAudioTrack && !TryReadString(ref buffer, out containerProbeInformation))
-        {
-            return false;
-        }
-
-        if (!TryReadInt64(ref buffer, out var startPositionValue))
-        {
-            return false;
-        }
-
-        var startPosition = startPositionValue is 0
-            ? default(TimeSpan?)
-            : TimeSpan.FromMilliseconds(startPositionValue);
-
-        var duration = durationValue == long.MaxValue
-            ? TimeSpan.MaxValue
-            : TimeSpan.FromMilliseconds(durationValue);
-
-        result = new LavalinkTrack
-        {
-            Author = author,
-            Identifier = identifier,
-            Title = title,
-            Duration = duration,
-            IsLiveStream = isStream,
-            IsSeekable = !isStream,
-            ProbeInfo = containerProbeInformation,
-            SourceName = sourceName,
-            StartPosition = startPosition,
-            Uri = uri,
-            ArtworkUri = artworkUri,
-            Isrc = isrc,
-            TrackData = originalTrackData.ToString(),
-        };
-
-        return true;
-    }
-
-    private static bool TryReadHeader(ref ReadOnlySpan<byte> buffer, out int version)
-    {
-        version = 1;
-
-        if (buffer.Length is < 4)
-        {
-            return false;
-        }
-
-        // the header is four bytes long, subtract
-        var header = BinaryPrimitives.ReadUInt32BigEndian(buffer);
-        buffer = buffer[4..];
-
-        var flags = (int)((header & 0xC0000000L) >> 30);
-        var hasVersion = (flags & 1) is not 0;
-
-        // verify size
-        var size = header & 0x3FFFFFFF;
-
-        if (size != buffer.Length)
-        {
-            // Invalid following payload length
-            return false;
-        }
-
-        if (hasVersion)
-        {
-            if (buffer.IsEmpty)
-            {
-                // Missing version
-                return false;
-            }
-
-            version = buffer[0];
-            buffer = buffer[1..];
-        }
-
-        // verify version
-        if (version is not 2 and not 3)
-        {
-            // unsupported version
-            return false;
-        }
-
-        return true;
-    }
-
-    private static bool TryReadBoolean(ref ReadOnlySpan<byte> buffer, out bool value)
-    {
-        if (buffer.IsEmpty)
-        {
-            value = default;
-            return false;
-        }
-
-        value = buffer[0] is not 0;
-        buffer = buffer[1..];
-        return true;
-    }
-
-    private static bool TryReadInt64(ref ReadOnlySpan<byte> buffer, out long value)
-    {
-        if (buffer.Length < 8)
-        {
-            value = default;
-            return false;
-        }
-
-        value = BinaryPrimitives.ReadInt64BigEndian(buffer);
-        buffer = buffer[8..];
-        return true;
-    }
-
-    private static bool TryReadOptionalString(ref ReadOnlySpan<byte> buffer, out string? value)
-    {
-        if (!TryReadBoolean(ref buffer, out var isPresent))
-        {
-            value = default;
-            return false;
-        }
-
-        if (!isPresent)
-        {
-            value = null;
-            return true;
-        }
-
-        return TryReadString(ref buffer, out value);
-    }
-
-    private static bool TryReadString(ref ReadOnlySpan<byte> buffer, [MaybeNullWhen(false)] out string value)
-    {
-        if (buffer.Length < 2)
-        {
-            value = default;
-            return false;
-        }
-
-        var length = BinaryPrimitives.ReadUInt16BigEndian(buffer);
-        buffer = buffer[2..];
-
-        if (buffer.Length < length)
-        {
-            value = default;
-            return false;
-        }
-
-        var stringBuffer = buffer[..length];
-        buffer = buffer[length..];
-
-        value = Encoding.UTF8.GetString(stringBuffer);
-        return true;
+        var trackDecoder = new LavalinkTrackDecoder(buffer);
+        return TryParse(originalTrackData, ref trackDecoder, out result);
     }
 
     public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out LavalinkTrack result)
