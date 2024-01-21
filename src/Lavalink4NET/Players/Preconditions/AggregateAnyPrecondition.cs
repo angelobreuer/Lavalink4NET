9774152920f309bd63b2ae99724b4cb873a8ff5b
Angelo Breuer <angelobr@outlook.de>
2024-01-21 21:36:44 +0100

    feat: Add aggregate preconditions

---
+++ src/Lavalink4NET/Players/Preconditions/AggregateAnyPrecondition.cs
@@ -0,0 +1,25 @@
+﻿namespace Lavalink4NET.Players.Preconditions;
+
+using System;
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal sealed class AggregateAnyPrecondition(ImmutableArray<IPlayerPrecondition> Preconditions) : IPlayerPrecondition
+{
+    public async ValueTask<bool> CheckAsync(ILavalinkPlayer player, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        ArgumentNullException.ThrowIfNull(player);
+
+        foreach (var precondition in Preconditions)
+        {
+            if (await precondition.CheckAsync(player, cancellationToken).ConfigureAwait(false))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- src/Lavalink4NET/Players/Preconditions/PlayerPrecondition.cs
+++ src/Lavalink4NET/Players/Preconditions/PlayerPrecondition.cs
@@ -1,5 +1,7 @@
 ﻿namespace Lavalink4NET.Players.Preconditions;
 
+using System.Collections.Immutable;
+
 public static class PlayerPrecondition
 {
     public static IPlayerPrecondition Playing { get; } = new SimplePrecondition(PlayerState.Playing);
@@ -17,4 +19,16 @@ public static class PlayerPrecondition
     public static IPlayerPrecondition HistoryEmpty { get; } = new HistoryEmptyPrecondition();
 
     public static IPlayerPrecondition HistoryNotEmpty { get; } = new HistoryNotEmptyPrecondition();
+
+    public static IPlayerPrecondition Any(ImmutableArray<IPlayerPrecondition> preconditions)
+        => new AggregateAnyPrecondition(preconditions);
+
+    public static IPlayerPrecondition Any(params IPlayerPrecondition[] preconditions)
+        => new AggregateAnyPrecondition(preconditions.ToImmutableArray());
+
+    public static IPlayerPrecondition All(ImmutableArray<IPlayerPrecondition> preconditions)
+        => new AggregateAllPrecondition(preconditions);
+
+    public static IPlayerPrecondition All(params IPlayerPrecondition[] preconditions)
+        => new AggregateAllPrecondition(preconditions.ToImmutableArray());
 }
