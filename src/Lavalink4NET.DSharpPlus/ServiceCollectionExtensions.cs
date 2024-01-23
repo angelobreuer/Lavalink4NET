2cadab8e20d90c35f5108ec3cad1790eaa813736
Lunar Starstrum <lunar@forsaken-borders.net>
2024-01-23 01:43:17 -0600

    Document ServiceCollectionExtensions

--- src/Lavalink4NET.DSharpPlus/ServiceCollectionExtensions.cs
+++ src/Lavalink4NET.DSharpPlus/ServiceCollectionExtensions.cs
@@ -4,8 +4,16 @@ using System;
 using Lavalink4NET.DSharpPlus;
 using Microsoft.Extensions.DependencyInjection;
 
+/// <summary>
+/// A collection of extension methods for <see cref="IServiceCollection"/>.
+/// </summary>
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds the Lavalink4NET DSharpPlus extension to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the extension to.</param>
+    /// <returns>The service collection for chaining.</returns>
     public static IServiceCollection AddLavalink(this IServiceCollection services)
     {
         ArgumentNullException.ThrowIfNull(services);
