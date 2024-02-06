5c5b1bddd268bc700ec6045c3ff9626cebfda2e0
Angelo Breuer <angelobr@outlook.de>
2024-02-06 23:38:32 +0100

    feat: Add helper constructors to TrackQueueItem

--- src/Lavalink4NET/Players/Queued/TrackQueueItem.cs
+++ src/Lavalink4NET/Players/Queued/TrackQueueItem.cs
@@ -1,3 +1,16 @@
 ﻿namespace Lavalink4NET.Players.Queued;
 
-public record class TrackQueueItem(TrackReference Reference) : ITrackQueueItem;
+using Lavalink4NET.Tracks;
+
+public record class TrackQueueItem(TrackReference Reference) : ITrackQueueItem
+{
+    public TrackQueueItem(LavalinkTrack track)
+        : this(new TrackReference(track))
+    {
+    }
+
+    public TrackQueueItem(string identifier)
+    : this(new TrackReference(identifier))
+    {
+    }
+}
