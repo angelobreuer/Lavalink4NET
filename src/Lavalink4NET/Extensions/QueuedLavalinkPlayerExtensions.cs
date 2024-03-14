536f68398aa787cd3838df5d8b0679c849515fa4
Angelo Breuer <angelobr@outlook.de>
2024-03-14 16:41:01 +0100

    feat: Add helper method to play TrackLoadResult directly

---
+++ src/Lavalink4NET/Extensions/QueuedLavalinkPlayerExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace Lavalink4NET.Extensions;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Lavalink4NET.Players.Queued;
+using Lavalink4NET.Rest.Entities.Tracks;
+
+public static class QueuedLavalinkPlayerExtensions
+{
+    public static async ValueTask<int> PlayAsync(this IQueuedLavalinkPlayer player, TrackLoadResult loadResult, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var queueOffset = default(int?);
+
+        if (loadResult.Playlist is null)
+        {
+            // Single track
+            queueOffset ??= await player
+              .PlayAsync(loadResult.Track!, cancellationToken: cancellationToken)
+              .ConfigureAwait(false);
+        }
+        else
+        {
+            // Playlist
+            foreach (var track in loadResult.Tracks)
+            {
+                queueOffset ??= await player
+                  .PlayAsync(track, cancellationToken: cancellationToken)
+                  .ConfigureAwait(false);
+            }
+        }
+
+        return queueOffset ?? throw new InvalidOperationException("No track is present.");
+    }
+}
