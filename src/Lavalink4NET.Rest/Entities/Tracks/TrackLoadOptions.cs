2e263156009bf8777f1feac7d4095f028b2d49cc
Angelo Breuer <angelobr@outlook.de>
2024-03-17 19:15:28 +0100

    refactor!: Remove compatibility constructor of TrackLoadOptions

--- src/Lavalink4NET.Rest/Entities/Tracks/TrackLoadOptions.cs
+++ src/Lavalink4NET.Rest/Entities/Tracks/TrackLoadOptions.cs
@@ -3,17 +3,4 @@
 public readonly record struct TrackLoadOptions(
     TrackSearchMode SearchMode = default,
     StrictSearchBehavior SearchBehavior = StrictSearchBehavior.Throw,
-    CacheMode CacheMode = CacheMode.Dynamic)
-{
-    // Constructor for compatibility
-    public TrackLoadOptions(
-        TrackSearchMode SearchMode = default,
-        bool? StrictSearch = null,
-        CacheMode CacheMode = CacheMode.Dynamic) : this(
-            SearchMode: SearchMode,
-            SearchBehavior: StrictSearch.GetValueOrDefault(true) ? StrictSearchBehavior.Throw : StrictSearchBehavior.Passthrough,
-            CacheMode: CacheMode)
-    {
-
-    }
-}
+    CacheMode CacheMode = CacheMode.Dynamic);
